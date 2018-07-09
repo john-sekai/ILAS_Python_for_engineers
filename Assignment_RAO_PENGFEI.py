103e93db492896413a3fafa0be48481db2ae713b
John Sekai <rpf1996@163.com>
2018-07-09 16:29:47 +0900

    20180709

--- Assignment_RAO_PENGFEI.py
+++ Assignment_RAO_PENGFEI.py
@@ -4,32 +4,37 @@ Created on Sun Jun 24 18:42:25 2018
 
 @author: rpf19
 """
+# libraries dependencies
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sympy import Symbol,diff,pprint
+from scipy.misc import derivative
+import sympy as sp
+import matplotlib.animation as animation
+from sympy.solvers.solveset import nonlinsolve
+
 url = 'https://raw.githubusercontent.com/justmarkham/DAT8/master/data/u.user'
 #　======================= Q1 =================================
-print(f'Now importing data from the following url: {url}')
+print(f'Now importing data from the following url: {url}\n')
 data = pd.read_csv(url,delimiter='|')
 print('Displaying data...')
 print(data.head())
-print()
-print('Counting each occupation...')
+print('\nCounting each occupation...')
 print(data['occupation'].value_counts())
-print()
 # ======================= Q2 ==================================
 print('\nNow entering Q2...')
-print('Removing occupation: other and retired...')
+print('Removing occupation: other and retired...\n')
 # remove the 2 required occupations
 data_clean = data[data.occupation!='other']
 data_clean = data_clean[data_clean.occupation!='retired']
 print('the clean data:')
 print(data_clean.head())
+print()
 print('Counting each occupation...')
 print(data_clean['occupation'].value_counts())
-print()
 # ======================= Q3 ==================================
-print('\nNow entering Q3:\nNow visualizing the occupation counts...')
+print('\nNow entering Q3:\n\nNow visualizing the occupation counts...\n')
 # get the count of each occupation
 job_data = data_clean['occupation'].value_counts() # job_data is a series
 # select the top 10 occupations as list
@@ -46,40 +51,46 @@ job_count = job_count[0:10]
 job_count.append(other)
 
 # print the top 10 occupations
-print(f'The top 10 occupations are:\n{job_list[0:10]}')
+print(f'The top 10 occupations are:\n{job_list[0:10]}\n')
 # print the count of other occupation
-print(f'the count of other occupation is: {other}')
+print(f'the count of other occupation is: {other}\n')
 
 print('Now plotting bar plot...')
 # create new plot
-plt.figure(figsize=(15,10))
+fig = plt.figure(figsize=(15,10))
+ax = fig.add_subplot(1,1,1)
 # create the position for displaying occupations in bar plot
 x_pos = np.arange(len(job_count))
 # bar plot 
 plt.bar(x_pos,job_count)
 # display the occupation names
 plt.xticks(x_pos,job_list,rotation=45,fontsize=20)
-
 # adding sticks, labels and title to the plot
 plt.yticks(fontsize=20)
+# adjusting the font size of the ticks in x and y axis
+for tick in ax.xaxis.get_major_ticks():
+    tick.label.set_fontsize(25) 
+for tick in ax.yaxis.get_major_ticks():
+    tick.label.set_fontsize(25) 
 plt.xlabel('Occupation',fontsize=30)
 plt.ylabel('Count',fontsize=30)
 plt.title('Statistics of occupations',fontsize=35)
 # show the plot before entering Q4
 plt.show()
 # ==================== Q4 ===============================
-print('\nNow entering Q4...')
+print('\nNow entering Q4...\n')
 # get the data with occupation the same as adminstrator
 admin = data_clean[data_clean.occupation == 'administrator']
+print('Printing the head of the data about administrator...')
 print(admin.head())
 # answer the question about mean and standard variance
-print(f'The mean age of administrator:{admin.age.mean()}')
-print(f'The standard deviation age of administrator:{admin.age.std()}')
-print()
+print(f'\nThe mean age of administrator:{admin.age.mean()}')
+print(f'The standard deviation age of administrator:{admin.age.std()}\n\n')
+
 # ================ Entering Part B =======================
-print('Now entering Part B...')
+print('Now entering Part B...\n')
 # ========================== Q1 ========================== 
-print('Now entering Q1...')
+print('Entering Q1...\nNow plotting the trajectory of the particle...\n')
 # time series
 t = np.linspace(0,2*np.pi,100)
 # some parameters given by the question
@@ -91,22 +102,29 @@ t_mark = [0,np.pi/2,np.pi*2]
 rx = lambda t: np.exp(-np.divide(t,T1))
 ry = lambda t: 2*np.cos(np.divide(t,T2))
 # generate the plot with size (15,10)
-plt.figure(figsize=(15,10))
+fig = plt.figure(figsize=(15,10))
+ax = fig.add_subplot(1,1,1)
+# adjusting the font size of the ticks in x and y axis
+for tick in ax.xaxis.get_major_ticks():
+    tick.label.set_fontsize(25) 
+for tick in ax.yaxis.get_major_ticks():
+    tick.label.set_fontsize(25) 
 # plot the trajectory of the particle
 plt.plot(rx(t),ry(t),'k-')
 # plot the marker for certain position
 plt.plot(rx(t_mark),ry(t_mark),'ro',markersize=10)
+
 # add some annotation to the plot, specifying the location of the annotation
 for ti in t_mark:
     plt.annotate(f't = {ti}',xy = (rx(ti),ry(ti)), xytext = (rx(ti)+0.02,ry(ti)+0.05),fontsize=15)
 # display the plot before entering Q2
+plt.title('The trajectory of the particle',fontsize = 30)
+plt.xlabel('$X$',fontsize=30)
+plt.ylabel('$Y$',fontsize=30)
 plt.show()
 # =========================== Q2 ==========================
-print('Now entering Q2...')
+print('Now entering Q2...\n')
 print('Q2 part a:')
-from sympy import Symbol,diff,pprint
-from scipy.misc import derivative
-import sympy as sp
 # specify the variable t
 t = Symbol('t')
 # claim the symbol function for derivative
@@ -117,8 +135,7 @@ a_x = (r_x).diff(t,t)
 a_y = (r_y).diff(t,t)
 # display the result
 pprint(f'the second order derivative of rx: {a_x}')
-pprint(f'the second order derivative of ry: {a_y}')
-print()
+pprint(f'the second order derivative of ry: {a_y}\n')
 print('Q2 part b:')
 # specify the time step
 dt = 100
@@ -147,29 +164,39 @@ a = lambda t:np.sqrt(np.add(np.square(derivative(rx,t,dx=1e-5,n=2)), np.square(d
 for t_index in np.arange(dt):
     at[t_index] = a(tspan[t_index])
 #plot the absolute acceleration
-plt.figure(figsize = (15,10))
+fig = plt.figure(figsize = (15,10))
+ax = fig.add_subplot(1,1,1)
+for tick in ax.xaxis.get_major_ticks():
+    tick.label.set_fontsize(25) 
+for tick in ax.yaxis.get_major_ticks():
+    tick.label.set_fontsize(25) 
 plt.plot(tspan,at_ref,'r-')
-plt.xlabel('Time(s)',fontsize=20)
-plt.ylabel('Acceleration Magnitude',fontsize=25)
-plt.title('Direct Method',fontsize=30)
+plt.xlabel('Time(s)',fontsize=30)
+plt.ylabel('Acceleration Magnitude',fontsize=30)
+plt.title('Direct Method Using Accleration Expression',fontsize=30)
 print('Plotting the acceleration plot...')
+plt.show()
 # the second plot,according to the second calculation method
-plt.figure(figsize = (15,10))
+fig = plt.figure(figsize=(15,10))
+ax = fig.add_subplot(1,1,1)
+for tick in ax.xaxis.get_major_ticks():
+    tick.label.set_fontsize(25) 
+for tick in ax.yaxis.get_major_ticks():
+    tick.label.set_fontsize(25) 
 plt.plot(tspan,at,'k-')
 plt.xlabel('Time(s)',fontsize=20)
 plt.ylabel('Acceleration Magnitude',fontsize=25)
 plt.title('Numerical Differentiation Method',fontsize=30)
 plt.show()
 #============================ Q3 ==========================
-print('Now entering Q3...') 
+print('\nNow entering Q3...\n') 
 print('Part A:')
-from sympy.solvers.solveset import nonlinsolve
 # evalute the velocity at t = 1 and convert it to numpy float
 v_x = np.array((sp.exp(-t/T1)).diff(t).subs(t,1.0)).astype(np.float64)
 v_y = np.array((2*sp.cos(t/T2)).diff(t).subs(t,1.0)).astype(np.float64)
 # calculate the magnitude of the velocity
 v = np.sqrt(np.add(np.square(v_x),np.square(v_y)))
-print(f'The magnitude velocity of the particle at time = 1s is:{v} m/s')
+print(f'The magnitude velocity of the particle at time = 1s is:{v} m/s\n')
 
 print('Part B:')
 # solve the nonlinear function of the r_y = 0 with the variable t
@@ -178,8 +205,7 @@ t_sol = nonlinsolve([r_y],[t])
 print('The time t when ry = 0 are:')
 pprint(t_sol)
 #============================ Q4 ==========================
-print('Now entering Q4...')
-import matplotlib.animation as animation
+print('\nNow entering Q4...')
 print('Animating the trajectory of the particle...')
 # 100 frames
 frame_num = 100
@@ -209,8 +235,6 @@ def animate(i):
     ax.plot(x,y,'r')
 # 50ms delay between frames to match timestep
 ani = animation.FuncAnimation(fig, animate ,frames = frame_num, interval = interval)
-# display the animation
-ani
 # save animation
 writer = animation.writers['ffmpeg'](fps=15,bitrate=1800)
 ani.save('assignment.mp4',writer = writer)
